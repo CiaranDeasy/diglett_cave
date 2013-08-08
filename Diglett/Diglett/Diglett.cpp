0ed2d23971b787eb9842ee87ce8ba6488ec2f08c
CiaranDeasy <cfd27@cam.ac.uk>
2013-08-08 22:41:05 +0100

    Added a source file!!!

---
+++ Diglett/Diglett/Diglett.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main() {
+	std::cout << "hello, world!\n";
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+	sf::RectangleShape square(sf::Vector2f(200.f,100.f));
+	sf::CircleShape nose(10.f);
+	square.move(0.f, 100.f);
+	nose.move(100.f, 75.f);
+	nose.setOrigin(10.f, 10.f);
+	nose.scale(3.f, 1.f);
+    shape.setFillColor(sf::Color(126, 64, 0, 255));
+    square.setFillColor(sf::Color(126, 64, 0, 255));
+    nose.setFillColor(sf::Color(255, 128, 160, 255));
+//HEllo
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.draw(square);
+        window.draw(nose);
+        window.display();
+    }
+}
