2e01afdccc886cbd8e6aba99a758d87e322fb568
CiaranDeasy <cfd27@cam.ac.uk>
2013-08-17 19:58:18 +0100

    Added a move method to Player.

--- Diglett/Diglett/Player.cpp
+++ Diglett/Diglett/Player.cpp
@@ -5,6 +5,10 @@ sf::Vector2f Player::getPosition() {
     return position;
 }
 
+void Player::move( float x, float y ) {
+    position = sf::Vector2f( position.x + x, position.y + y );
+}
+
 Player::Player(void) {
     position = sf::Vector2f( 1.0f, 1.0f );
 }
--- Diglett/Diglett/Player.h
+++ Diglett/Diglett/Player.h
@@ -3,6 +3,7 @@
 class Player {
 public:
     sf::Vector2f getPosition();
+    void move( float x, float y );
     Player(void);
     ~Player(void);
 private:
