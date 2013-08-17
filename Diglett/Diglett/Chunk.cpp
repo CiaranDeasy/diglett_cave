63160700373f622c9901bf69c3b79cfa3ee2fb2d
CiaranDeasy <cfd27@cam.ac.uk>
2013-08-17 20:00:05 +0100

    Made the tiles array of a Chunk private, added a get method for getting tiles. Added a field indicating the tile index of the bottom-right tile in the chunk, relative to the whole world, a get method to retrieve it, and a constructor argument to set it.

--- Diglett/Diglett/Chunk.cpp
+++ Diglett/Diglett/Chunk.cpp
@@ -1,6 +1,15 @@
 #include "Chunk.h"
 
-Chunk::Chunk( Tile::Type type ) {
+sf::Vector2i Chunk::getPosition() {
+    return position;
+}
+
+Tile Chunk::getTile( int x, int y ) {
+    return tiles[x][y];
+}
+
+Chunk::Chunk( sf::Vector2i pos, Tile::Type type ) {
+    position = pos;
     for( int i = 0; i < CHUNK_SIDE; i++ ) {
         for( int j = 0; j < CHUNK_SIDE; j++ ) {
             tiles[i][j] = Tile( type );
--- Diglett/Diglett/Chunk.h
+++ Diglett/Diglett/Chunk.h
@@ -1,18 +1,26 @@
 #pragma once
 #include "Tile.h"
+#include <SFML/System.hpp>
 const int CHUNK_SIDE = 20;
 
 class Chunk {
-    Tile tiles[CHUNK_SIDE][CHUNK_SIDE];
 public:
+    sf::Vector2i getPosition();
+    Tile getTile( int x, int y );
+
     // Constructor which takes a type of tile and makes a chunk consisting 
     // entirely of tiles of that type.
-    Chunk( Tile::Type );
+    Chunk( sf::Vector2i, Tile::Type );
     ~Chunk( void );
 
     // Default constructor not supported.
     // Present to keep arrays happy.
     Chunk( void );
 private:
+    // Tiles in the chunk.
+    Tile tiles[CHUNK_SIDE][CHUNK_SIDE];
+
+    // Tile index of tiles[0][0]
+    sf::Vector2i position;
 };
 
