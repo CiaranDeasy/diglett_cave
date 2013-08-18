497ee99cdd2e6a7c1b1f6ce2ef11ba9270c7775d
CiaranDeasy <cfd27@cam.ac.uk>
2013-08-18 11:21:50 +0100

    The offset values were incorrect, and have been fixed. The default constructor for WorldData was setting the position values of Chunks incorrectly, and this has been fixed.

--- Diglett/Diglett/WorldData.cpp
+++ Diglett/Diglett/WorldData.cpp
@@ -7,16 +7,18 @@ Chunk WorldData::getChunk( int x, int y ) {
 }
 
 WorldData::WorldData(void) {
-    xOffset = -10;
-    yOffset = -10;
+    xOffset = 10;
+    yOffset = 10;
     for( int i = 0; i < 20; i++ ) {
         for( int j = 0; j < 10; j++ ) {
             chunks[i][j] = Chunk::Chunk( 
-                    sf::Vector2i( i + xOffset, j + yOffset ), 
-                    Tile::Type::Surface );
-            chunks[i][j+10] = Chunk::Chunk( 
-                    sf::Vector2i( i + xOffset, j + yOffset ), 
+                    sf::Vector2i( ( i - xOffset ) * CHUNK_SIDE, 
+                    ( j - yOffset ) * CHUNK_SIDE ),
                     Tile::Type::Dirt );
+            chunks[i][j+10] = Chunk::Chunk( 
+                    sf::Vector2i( ( i - xOffset ) * CHUNK_SIDE, 
+                    ( j + 10 - yOffset ) * CHUNK_SIDE ),
+                    Tile::Type::Surface );
         }
     }
 }
