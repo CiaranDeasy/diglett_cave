b4c6e7eec5beea814857edb6713d60c162d8aac5
CiaranDeasy <cfd27@cam.ac.uk>
2013-08-28 20:35:50 +0100

    Removed the roundToMoreNegative() utility function, replaced calls to it with calls to the standard floor() function.

--- Diglett/Diglett/Utility.cpp
+++ Diglett/Diglett/Utility.cpp
@@ -1,24 +1,15 @@
 #include "Utility.h"
 #include "Constants.h"
 
-// For co-ordinate conversion from world co-ordinates to chunk co-ordinates, we
-// need to always round towards negative infinity. C++ always rounds towards 
-// zero. Today we correct that mistake.
-int Utility::roundToMoreNegative( float f ) {
-    if( f > 0 ) return (int) f;
-    else return ( (int) f ) - 1;
-}
-
 // Converts a point in game co-ordinates to a tile index.
 sf::Vector2i Utility::coordsGameToTile( sf::Vector2f in ) {
-    return sf::Vector2i( roundToMoreNegative( in.x ), 
-            roundToMoreNegative( in.y ) );
+    return sf::Vector2i( floor( in.x ), floor( in.y ) );
 }
 
 // Converts a point in game co-ordinates to a chunk index.
 sf::Vector2i Utility::coordsGameToChunk( sf::Vector2f in ) {
-    return sf::Vector2i( roundToMoreNegative( in.x / CHUNK_SIDE ), 
-            roundToMoreNegative( in.y / CHUNK_SIDE ) );
+    return sf::Vector2i( floor( in.x / CHUNK_SIDE ), 
+        floor( in.y / CHUNK_SIDE ) );
 }
 
 // Converts a point in game co-ordinates to a point in window (drawing) 
