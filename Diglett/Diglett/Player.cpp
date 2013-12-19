283cffc8cf1f974871173e83724514d111df29c2
CiaranDeasy <cfd27@cam.ac.uk>
2013-12-19 02:14:04 +0000

    Added CLIPPING!!!!!

--- Diglett/Diglett/Player.cpp
+++ Diglett/Diglett/Player.cpp
@@ -1,5 +1,7 @@
 #include "Player.h"
 #include <iostream>
+#include "Tile.h"
+#include "WorldData.h"
 
 Player& Player::getPlayer() {
     return singleton;
@@ -15,13 +17,74 @@ void Player::move( float x, float y ) {
     if( position.x + x > 150 || position.x + x < -150 
         || position.y + y > 150 || position.y + y < -150 ) 
         return;
-    position = sf::Vector2f( position.x + x, position.y + y );
+    // Check for collisions.
+    float oldX = position.x;
+    float oldY = position.y;
+    float newX = oldX + x;
+    float newY = oldY + y;
+    // Check for clipping with tiles to the left.
+    if( floor( oldX + leftClip ) > floor( newX + leftClip ) ) {
+        Tile topTile = WorldData::getWorldData().getTile( 
+            floor( newX + leftClip ), floor( oldY + topClip ) );
+        Tile bottomTile = WorldData::getWorldData().getTile( 
+            floor( newX + leftClip ), floor( oldY + bottomClip ) );
+        if( ( topTile.getType() != Tile::Air && 
+            topTile.getType() != Tile::Surface ) 
+            || ( bottomTile.getType() != Tile::Air && 
+            bottomTile.getType() != Tile::Surface ) ) {
+            newX = floor( oldX ) - leftClip;
+        }
+    }
+    // Check for clipping with tiles to the right.
+    if( floor( oldX + rightClip ) < floor( newX + rightClip ) ) {
+        Tile topTile = WorldData::getWorldData().getTile( 
+            floor( newX + rightClip ), floor( oldY + topClip ) );
+        Tile bottomTile = WorldData::getWorldData().getTile( 
+            floor( newX + rightClip ), floor( oldY + bottomClip ) );
+        if( ( topTile.getType() != Tile::Air && 
+            topTile.getType() != Tile::Surface ) 
+            || ( bottomTile.getType() != Tile::Air && 
+            bottomTile.getType() != Tile::Surface ) ) {
+            newX = floor( oldX + 1 ) - 0.001 - rightClip;
+        }
+    }
+    // Check for clipping with tiles above.
+    if( floor( oldY + topClip ) < floor( newY + topClip ) ) {
+        Tile leftTile = WorldData::getWorldData().getTile( 
+            floor( newX + leftClip ), floor( newY + topClip ) );
+        Tile rightTile = WorldData::getWorldData().getTile( 
+            floor( newX + rightClip ), floor( newY + topClip ) );
+        if( ( leftTile.getType() != Tile::Air && 
+            leftTile.getType() != Tile::Surface ) 
+            || ( rightTile.getType() != Tile::Air && 
+            rightTile.getType() != Tile::Surface ) ) {
+            newY = floor( oldY + 1 ) - 0.001 - topClip;
+        }
+    }
+    // Check for clipping with tiles below.
+    if( floor( oldY + bottomClip ) > floor( newY + bottomClip ) ) {
+        Tile& leftTile = WorldData::getWorldData().getTile( 
+            floor( newX + leftClip ), floor( newY + bottomClip ) );
+        Tile& rightTile = WorldData::getWorldData().getTile( 
+            floor( newX + rightClip ), floor( newY + bottomClip ) );
+        if( ( leftTile.getType() != Tile::Air && 
+            leftTile.getType() != Tile::Surface ) 
+            || ( rightTile.getType() != Tile::Air && 
+            rightTile.getType() != Tile::Surface ) ) {
+            newY = floor( oldY ) - bottomClip;
+        }
+    }
+    position = sf::Vector2f( newX, newY );
 }
 
 Player Player::singleton = Player();
 
 Player::Player(void) {
     position = sf::Vector2f( 1.0f, 1.0f );
+    topClip = 0.45;
+    bottomClip = -0.45;
+    leftClip = -0.45;
+    rightClip = 0.45;
 }
 
 
--- Diglett/Diglett/Player.h
+++ Diglett/Diglett/Player.h
@@ -8,6 +8,13 @@ public:
     ~Player(void);
 private:
     static Player singleton;
+    // The player's position is their centre. Adding a "Clip" to the 
+    // appropriate component of the position gives a line of the player's 
+    // bounding box.
     sf::Vector2f position;
+    float topClip;
+    float bottomClip;
+    float leftClip;
+    float rightClip;
     Player(void);
 };
