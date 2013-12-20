fb08830ae27032221baf522b567f5ffe2b2e8921
CiaranDeasy <cfd27@cam.ac.uk>
2013-12-20 18:26:22 +0000

    Added a diggable() method for testing whether a Tile is diggable, and an array to hold information about which Tile types are diggable.

--- Diglett/Diglett/Tile.cpp
+++ Diglett/Diglett/Tile.cpp
@@ -12,3 +12,15 @@ Tile::~Tile(void) {
 }
 
 Tile::Type Tile::getType() { return type; }
+
+void Tile::dig() { type = Air; }
+
+bool Tile::diggable(Tile t) {
+    return diggableByType[t.getType()];
+}
+
+bool Tile::diggableByType[Count] = {
+    true, // Dirt
+    false, // Air
+    false // Surface
+};
--- Diglett/Diglett/Tile.h
+++ Diglett/Diglett/Tile.h
@@ -6,13 +6,18 @@ public:
     enum Type {
         Dirt,
         Air,
-        Surface
+        Surface,
+        // Newly added types must be added to the diggableByType array!
+        Count // A fake type whose value is the no. of elements in the enum.
     };
     Tile(void);
     Tile(Type type);
     ~Tile(void);
     Type getType();
+    void dig();
+    static bool diggable(Tile t);
 
 private:
     Type type;
+    static bool diggableByType[Count];
 };
