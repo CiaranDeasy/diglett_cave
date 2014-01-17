900b328627da5b60644fd7d51fd360708277c001
CiaranDeasy <cfd27@cam.ac.uk>
2014-01-17 22:15:25 +0000

    Wrote a TextPopUp class for creating text pop-ups on-screen.

---
+++ Diglett/Diglett/TextPopUp.cpp
@@ -0,0 +1,39 @@
+#include "TextPopUp.h"
+#include <iostream>
+
+TextPopUp::TextPopUp( std::string message, sf::Font& font ) : font(font) {
+    this->message = message;
+}
+
+TextPopUp::~TextPopUp(void) {
+}
+
+void TextPopUp::draw( 
+        sf::RenderTarget& target, 
+        sf::RenderStates states ) const {
+    // Determine the position vector.
+    sf::Vector2i position = sf::Vector2i( 
+            target.getSize().x / 2, target.getSize().y / 4 );
+    // Make the text.
+    sf::Text text = sf::Text( message, font, TEXT_SIZE );
+    sf::FloatRect bounds = text.getLocalBounds();
+    text.setOrigin( bounds.left + bounds.width / 2, 
+            bounds.top + bounds.height / 2 );
+    text.setPosition( position.x, position.y );
+    // Draw the background.
+    sf::RectangleShape background = sf::RectangleShape( sf::Vector2f( 
+            bounds.width + 2*BORDER_SIZE, bounds.height + 2*BORDER_SIZE ) );
+    background.setOrigin( 
+            background.getSize().x /2, background.getSize().y /2 );
+    background.setFillColor( BACKGROUND_COLOR );
+    background.setPosition( position.x, position.y );
+    target.draw( background );
+    // Draw the text.
+    target.draw( text );
+}
+
+const int TextPopUp::BORDER_SIZE = 10;
+const int TextPopUp::TEXT_SIZE = 18;
+const sf::Color TextPopUp::BACKGROUND_COLOR = sf::Color( 0, 0, 0, 255 );
+const sf::Color TextPopUp::TEXT_COLOR = 
+        sf::Color( 255, 255, 255, 255 );
---
+++ Diglett/Diglett/TextPopUp.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+/* An instance of this class represents an interface element displaying the 
+   player's current hull strength.
+*/
+
+class TextPopUp : public sf::Drawable {
+public:
+
+    TextPopUp( std::string message, sf::Font& font );
+    ~TextPopUp();
+
+    // Draws the pop-up to the given RenderTarget.
+    virtual void draw( 
+            sf::RenderTarget& target, sf::RenderStates states ) const;
+
+private: 
+    
+    // The number of pixels between the text and the edges of the pop-up.
+    static const int BORDER_SIZE;
+
+    // The position of the game window is the centre of the top half of the 
+    // window.
+
+    static const int TEXT_SIZE;
+    static const sf::Color BACKGROUND_COLOR;
+    static const sf::Color TEXT_COLOR;
+
+    sf::Font& font;
+    std::string message;
+
+};
+
